be55b168d971858f799631864a3f9a6f6ab74c7b
ravabasya06 <ravabasya06@gmail.com>
2024-07-30 14:39:57 +0700

    added seeder for spbu

--- database/factories/SpbuFactory.php
+++ database/factories/SpbuFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Spbu>
@@ -17,7 +18,11 @@ class SpbuFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => 'Pertamina ' . fake()->city(),
+            'road' => fake()->address(),
+            'city' => fake()->city(),
+            'province' => fake()->state(),
+            'island' => fake()->word(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Spbu;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Spbu::factory()->create([
+        'name' => 'Pertamina Bekasi',
+        'road' => 'Jl. Galaxy',
+        'city' => 'Bekasi Selatan',
+        'province' => 'Bekasi',
+        'island' => 'Jawa Barat',
+        ]);
+        Spbu::factory(1)->create();
     }
 }
