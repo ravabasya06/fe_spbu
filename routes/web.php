72f6a93e48f20ab2372c178e8822b8e8438b23e7
ravabasya06 <ravabasya06@gmail.com>
2024-07-31 17:02:12 +0700

    added new routes

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SpbuController;
-use App\Models\Spbu;
+use App\Http\Controllers\AnalysisController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
-Route::get('/dashboard', [PagesController::class, 'dashboard']);
-Route::get('/analysis', [SpbuController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/analysis', [AnalysisController::class, 'index']);
+Route::get('/spbu/{id}', [SpbuController::class, 'index']);
 
-Route::get('/spbu/{id}', [SpbuController::class, 'show']);
 Route::get('/users', [UserController::class, 'index'])->name('user.index');
 Route::post('/users', [UserController::class, 'store']);
 Route::get('/users/register', [UserController::class, 'register']);
