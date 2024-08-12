aa70a2ab9ad586f1ca91b83fe86d111819d9b017
ravabasya06 <ravabasya06@gmail.com>
2024-08-12 16:53:29 +0700

    removed email as fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
     ];
 
@@ -56,7 +55,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
