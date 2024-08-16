d90ec2de3dddd69d19832d8552d2db667e120731
ravabasya06 <ravabasya06@gmail.com>
2024-08-16 15:26:51 +0700

    fixed not able to logout

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Profile');
+        return Inertia::render('Profile', [
+        ]);
     }
 
     public function logout(Request $request)
