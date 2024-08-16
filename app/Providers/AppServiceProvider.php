4142bb7464d2f6022ddaa688fb449e568880f762
ravabasya06 <ravabasya06@gmail.com>
2024-08-16 15:23:26 +0700

    added user to global shared data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Inertia::share([
+            'user' => function () {
+                return Auth::user();
+            },
+        ]);
     }
 }
