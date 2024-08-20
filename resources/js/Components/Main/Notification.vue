185ab1ea30caf93f06f54f5aba1d0fba24371694
Adreon <sachioramzy12@gmail.com>
2024-08-20 14:07:24 +0700

    sizing and adding an icon in the notification

--- resources/js/Components/Main/Notification.vue
+++ resources/js/Components/Main/Notification.vue
@@ -1,10 +1,19 @@
 <template>
     <div v-if="$page.props.flash.message" class="alert">
+        <font-awesome-icon
+            icon="circle-info"
+            size="lg"
+            style="color: #ffffff"
+        />
         {{ $page.props.flash.message }}
     </div>
 </template>
 <style scope>
 .alert {
+    left: 10px;
+    top: 10px;
+    width: max-content;
+    border-radius: 10px;
     color: white;
     background-color: black;
 }
