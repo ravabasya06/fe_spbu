d561bd477e1a4db5934830aa81e53c1a59c32de7
ravabasya06 <ravabasya06@gmail.com>
2024-08-20 17:35:21 +0700

    removed unused stuff

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Layout from "../Components/Main/Layout.vue";
-import Search from "../Components/Analysis/Search.vue";
 import AnalysisCard from "../Components/Analysis/AnalysisCard.vue";
 
 defineProps(["spbus", "query", "results"]);
@@ -8,26 +7,6 @@ defineProps(["spbus", "query", "results"]);
 
 <template>
     <Layout title="Analysis">
-        <div class="search-wrapper">
-            <Search />
-        </div>
-        <div class="list">
-            <AnalysisCard :spbus="spbus" :results="results" />
-        </div>
+        <AnalysisCard :spbus="spbus" :results="results" />
     </Layout>
 </template>
-
-<style scoped>
-.search-wrapper {
-    display: flex;
-    justify-content: center;
-}
-.navigation {
-    position: absolute;
-    padding: 10px;
-    border-radius: 5px;
-    z-index: 10;
-    color: white;
-    text-decoration: none;
-}
-</style>
