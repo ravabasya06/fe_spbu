8c98bcaba6d1b717667e4c6fba1ffd0457a7e3bd
ravabasya06 <ravabasya06@gmail.com>
2024-08-20 13:31:35 +0700

    added notification

---
+++ resources/js/Components/Main/Notification.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="$page.props.flash.message" class="alert">
+        {{ $page.props.flash.message }}
+    </div>
+</template>
+<style scope>
+.alert {
+    color: white;
+    background-color: black;
+}
+</style>
