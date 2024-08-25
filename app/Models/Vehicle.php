7bab13072b76e836c537e4ab477f385e9dd65be3
ravabasya06 <ravabasya06@gmail.com>
2024-08-25 22:00:31 +0700

    fixed created_at format not working

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Carbon\Carbon;
 
 class Vehicle extends Model
 {
