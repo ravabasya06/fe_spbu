10b57f980d20a33513229a26d6a5c99976f58027
ravabasya06 <ravabasya06@gmail.com>
2024-08-29 18:17:21 +0700

    added more routes

--- routes/admin/detection.php
+++ routes/admin/detection.php
@@ -3,5 +3,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DetectionController;
 
 Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/detection/{id}/edit', [DetectionController::class, 'edit']);
+    Route::put('/detection/{id}', [DetectionController::class, 'update']);
     Route::delete('/detection/{id}', [DetectionController::class, 'destroy']);  
+    Route::post('/detection', [detectionController::class, 'store'])->name('detection.store');
+    Route::get('/spbu/{id}/detection', [DetectionController::class, 'create'])->name('detection.create');
 });
