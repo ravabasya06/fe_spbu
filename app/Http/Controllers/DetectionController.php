9283ddf426b0d92aea735fdc3afba06396cf3fd6
ravabasya06 <ravabasya06@gmail.com>
2024-08-29 18:17:43 +0700

    added create method

--- app/Http/Controllers/DetectionController.php
+++ app/Http/Controllers/DetectionController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use App\Models\Spbu;
 use App\Models\Detection;
 
 class DetectionController extends Controller
 {
+    public function create($spbu_id){
+        $cctvs = $this->fetchModel('cctvs', $spbu_id)->orderBy("cctv_number")->get();
+        return Inertia::render('Admin/DetectionForm', [
+            'spbu_id' => $spbu_id,
+            'cctvs' => $cctvs
+        ]);
+    }
+
     public function destroy($detection_id){
         $detection = Detection::find($detection_id);
         Detection::destroy($detection_id);
         return back()->with('message', 'This Detection is successfully deleted!');
+    }
+
+    public function fetchModel($model, $spbu_id)
+    {
+        return DB::table($model)->where('spbu_id', $spbu_id)->orderByRaw('created_at ASC');
     }
 }
