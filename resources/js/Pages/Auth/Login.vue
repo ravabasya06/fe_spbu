2a8d5c485f54a67ace34b811befeb5140061eb6b
ravabasya06 <ravabasya06@gmail.com>
2024-08-29 14:11:23 +0700

    added layout

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
 import Button from "../../Components/Main/Button.vue";
+import Layout from "../../Components/Main/Layout.vue";
 
 const form = useForm({
     name: "",
@@ -13,49 +14,51 @@ const submit = () => {
 </script>
 
 <template>
-    <div class="login">
-        <div class="login-container">
-            <h2>Login</h2>
-            <form @submit.prevent="submit">
-                <div>
-                    <label for="name">Name</label>
-                    <input
-                        v-model="form.name"
-                        id="name"
-                        type="text"
-                        required
-                        autocomplete="off"
-                        autofocus
+    <Layout title="Login">
+        <div class="login">
+            <div class="login-container">
+                <h2>Login</h2>
+                <form @submit.prevent="submit">
+                    <div>
+                        <label for="name">Name</label>
+                        <input
+                            v-model="form.name"
+                            id="name"
+                            type="text"
+                            required
+                            autocomplete="off"
+                            autofocus
+                        />
+                        <span
+                            v-if="form.errors.name"
+                            style="color: red; text-align: center"
+                            >{{ form.errors.name }}</span
+                        >
+                    </div>
+                    <div>
+                        <label for="password">Password</label>
+                        <input
+                            v-model="form.password"
+                            id="password"
+                            type="password"
+                            required
+                        />
+                        <span
+                            v-if="form.errors.password"
+                            style="color: red; text-align: center"
+                            >{{ form.errors.password }}</span
+                        >
+                    </div>
+                    <Button
+                        type="submit"
+                        value="Login"
+                        color="blue"
+                        :disabled="form.processing"
                     />
-                    <span
-                        v-if="form.errors.name"
-                        style="color: red; text-align: center"
-                        >{{ form.errors.name }}</span
-                    >
-                </div>
-                <div>
-                    <label for="password">Password</label>
-                    <input
-                        v-model="form.password"
-                        id="password"
-                        type="password"
-                        required
-                    />
-                    <span
-                        v-if="form.errors.password"
-                        style="color: red; text-align: center"
-                        >{{ form.errors.password }}</span
-                    >
-                </div>
-                <Button
-                    type="submit"
-                    value="Login"
-                    color="blue"
-                    :disabled="form.processing"
-                />
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
+    </Layout>
 </template>
 
 <style scoped>
