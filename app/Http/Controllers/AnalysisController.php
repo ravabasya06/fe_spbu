c0b062aacdded1ad327350207f1995292adb36c0
ravabasya06 <ravabasya06@gmail.com>
2024-09-02 13:21:26 +0700

    fixed controller for search not using the right column

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -18,12 +18,18 @@ class AnalysisController extends Controller
     }
     public function search(Request $request){
         $query = $request->input('search_query');
-        $results = Spbu::where('spbu_id', 'LIKE', "%{$query}%")
-            ->orWhere('name', 'LIKE', "%{$query}%")
-            ->orWhere('road', 'LIKE', "%{$query}%")
-            ->orWhere('city', 'LIKE', "%{$query}%")
-            ->orWhere('province', 'LIKE', "%{$query}%")
-            ->orWhere('island', 'LIKE', "%{$query}%")
+        $spbu = Spbu::join('islands', 'spbus.island_id', '=', 'islands.island_id')
+        ->join('provinces', 'spbus.province_id', '=', 'provinces.province_id')
+        ->select('spbus.spbu_id', 'spbus.name', 'spbus.road', 'spbus.city',
+         'provinces.name as province_name', 'islands.name as island_name');
+         
+        $results = $spbu
+            ->where('spbus.spbu_id', 'LIKE', "%{$query}%")
+            ->orWhere('spbus.name', 'LIKE', "%{$query}%")
+            ->orWhere('spbus.road', 'LIKE', "%{$query}%")
+            ->orWhere('spbus.city', 'LIKE', "%{$query}%")
+            ->orWhere('provinces.name', 'LIKE', "%{$query}%")
+            ->orWhere('islands.name', 'LIKE', "%{$query}%")
             ->get();
         
         return Inertia::render('Analysis', [
