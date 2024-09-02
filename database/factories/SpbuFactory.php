f5dca3654255e7863130a6f1f11e6150324faa87
ravabasya06 <ravabasya06@gmail.com>
2024-09-02 11:07:23 +0700

    added island and province to factory

--- database/factories/SpbuFactory.php
+++ database/factories/SpbuFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as FakerFactory;
+use App\Models\Island;
+use App\Models\Province;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Spbu>
@@ -15,27 +17,18 @@ class SpbuFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    
-    protected static $islands = [
-        'Jawa', 'Sumatera', 'Kalimantan', 'Sulawesi', 'Papua'
-    ];
-
-    protected function island()
-    {
-        return collect(static::$islands)->random();
-    }
 
     public function definition(): array
     {  
         $faker = FakerFactory::create('id_ID');
         $street = $faker->street();
-        $state = $faker->state();
+        $province = Province::inRandomOrder()->first();
         return [
             'name' => 'Pertamina ' . $street,
             'road' => 'Jl. ' . $street . ' No. ' . $faker->numberBetween(1, 80),
             'city' => $faker->city(),
-            'province' => $state,
-            'island' => self::island(),
+            'province_id' => $province->province_id,
+            'island_id' => $province->island_id,
         ];
     }
 }
