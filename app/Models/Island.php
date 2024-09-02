78796e32682b5b0b563ac1df0305b15084e899a8
ravabasya06 <ravabasya06@gmail.com>
2024-09-02 11:02:11 +0700

    added island and province model

---
+++ app/Models/Island.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Island extends Model
+{
+    use HasFactory;
+
+    public function provinces()
+    {
+        return $this->hasMany(Province::class, 'island_id', 'island_id');
+    }
+    
+    public function spbus()
+    {
+        return $this->hasMany(Spbu::class, 'island_id', 'island_id');
+    }
+}
---
+++ app/Models/Province.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Province extends Model
+{
+    use HasFactory;
+
+    public function islands()
+    {
+        return $this->belongsTo(Island::class, 'island_id', 'island_id');
+    }
+
+    public function spbus()
+    {
+        return $this->hasMany(Spbu::class, 'province_id', 'province_id');
+    }
+}
