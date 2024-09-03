56b1511963170a83d096ef27dcc3ac82909f15e2
Adreon <sachioramzy12@gmail.com>
2024-09-03 17:40:20 +0700

    coordianting each island

--- resources/js/Components/Dashboard/Map.vue
+++ resources/js/Components/Dashboard/Map.vue
@@ -3,10 +3,18 @@ import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 import { ref } from "vue";
 import "leaflet/dist/leaflet.css";
 const zoom = ref(5);
-const center = ref([-0.5143059767194057, 120.73280216006904]);
+const center = ref([-0.5, 120.7]);
 const markerLatLng = ref({
-    marker1: [-0.5143059767194057, 111.73280216006904],
-    marker2: [-0.5143059767192034, 100.73280216757332],
+    Kalimantan1: [-1, 110],
+    Kalimantan2: [4.3, 117],
+    Sumatera1: [5.6, 95.4],
+    Sumatera2: [-5.4, 105.3],
+    Jawa1: [-8, 114.3],
+    Jawa2: [-6.8, 105.3],
+    Sulawesi1: [-1, 120],
+    Sulawesi2: [-5.3, 119.4],
+    Papua1: [-2, 137.5],
+    Papua2: [-9, 140.9],
 });
 </script>
 
