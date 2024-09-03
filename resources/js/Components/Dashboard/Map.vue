79231ef90acf74f157e153cc7b1f97be80151bdd
Adreon <sachioramzy12@gmail.com>
2024-09-03 16:10:11 +0700

    adding vueleaflet for displaying interactive map

---
+++ resources/js/Components/Dashboard/Map.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { ref } from "vue";
+import "leaflet/dist/leaflet.css";
+const zoom = ref(5);
+const center = ref([-0.5143059767194057, 120.73280216006904]);
+const markerLatLng = ref({
+    marker1: [-0.5143059767194057, 111.73280216006904],
+    marker2: [-0.5143059767192034, 100.73280216757332],
+});
+</script>
+
+<template>
+    <div class="map-container">
+        <l-map
+            ref="map"
+            v-model:zoom="zoom"
+            v-model:center="center"
+            :useGlobalLeaflet="false"
+        >
+            <l-tile-layer
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+                layer-type="base"
+                name="Stadia Maps Basemap"
+            ></l-tile-layer>
+            <l-marker v-for="lat in markerLatLng" :lat-lng="lat"></l-marker>
+        </l-map>
+    </div>
+</template>
+
+<style scoped>
+.map-container {
+    height: 90vh;
+    width: 100vw;
+    z-index: 1;
+}
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,17 +2,14 @@
 import Layout from "../Components/Main/Layout.vue";
 import DashboardData from "../Components/Dashboard/DashboardData.vue";
 import Button from "../Components/Main/Button.vue";
+import Map from "../Components/Dashboard/Map.vue";
 defineProps(["islands", "provinces"]);
 </script>
 
 <template>
     <Layout title="Dashboard">
         <div class="map-container">
-            <img
-                src="../../../public/images/map.jpg"
-                alt="Map of Indonesia"
-                class="map"
-            />
+            <Map />
         </div>
         <div class="navigation">
             <Button type="link" href="/" value="Back" color="blue" />
@@ -29,7 +26,7 @@ defineProps(["islands", "provinces"]);
     transform: translate(-40%, -50%);
     padding: 10px;
     border-radius: 5px;
-    z-index: 10;
+    z-index: 5;
     color: white;
     text-decoration: none;
 }
