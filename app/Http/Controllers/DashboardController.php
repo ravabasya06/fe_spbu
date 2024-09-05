c1350657efefcfe248490ecb74388d9998acdeba
ravabasya06 <ravabasya06@gmail.com>
2024-09-05 17:14:39 +0700

    removed foreach updates on islands and provinces, improved the code to remove count column

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,16 +5,31 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
-use App\Models\Spbu;
-use App\Models\Island;
-use App\Models\Province;
-
 
 class DashboardController extends Controller
 {
     public function index(){
-        $islands = Island::select('island_id', 'name', 'count')->get();
-        $provinces = Province::select('province_id', 'name', 'island_id', 'count')->get();
+        $islands = DB::table('islands')
+        ->leftJoin('spbus', 'islands.island_id', '=', 'spbus.island_id')
+        ->select(
+            'islands.island_id', 
+            'islands.name', 
+            DB::raw('COUNT(spbus.spbu_id) as spbu_count')
+        )
+        ->groupBy('islands.island_id', 'islands.name')
+        ->get();
+
+        $provinces = DB::table('provinces')
+        ->leftJoin('spbus', 'provinces.province_id', '=', 'spbus.province_id')
+        ->select(
+            'provinces.province_id', 
+            'provinces.name',
+            'provinces.island_id',
+            DB::raw('COUNT(spbus.spbu_id) as spbu_count')
+        )
+        ->groupBy('provinces.province_id', 'provinces.name', 'provinces.island_id')
+        ->get();
+
         $spbus = DB::table('spbus')
             ->join('vehicles', 'spbus.spbu_id', '=', 'vehicles.spbu_id')
             ->select(
@@ -23,11 +38,11 @@ class DashboardController extends Controller
                 'spbus.latitude', 
                 'spbus.longitude',
                 //today
-                DB::raw('count(CASE WHEN DATE(vehicles.created_at) = CURDATE() THEN 1 END) as today_vehicle_count'),
-                DB::raw('count(CASE WHEN vehicles.type_vehicle_id = 1 AND DATE(vehicles.created_at) = CURDATE() THEN 1 END) as today_motor_count'),
-                DB::raw('count(CASE WHEN vehicles.type_vehicle_id = 2 AND DATE(vehicles.created_at) = CURDATE() THEN 1 END) as today_car_count'),
-                DB::raw('count(CASE WHEN vehicles.type_vehicle_id = 3 AND DATE(vehicles.created_at) = CURDATE() THEN 1 END) as today_bus_count'),
-                DB::raw('count(CASE WHEN vehicles.type_vehicle_id = 4 AND DATE(vehicles.created_at) = CURDATE() THEN 1 END) as today_truck_count'),
+                DB::raw('SUM(CASE WHEN DATE(vehicles.created_at) = CURDATE() THEN 1 ELSE 0 END) as today_vehicle_count'),
+                DB::raw('SUM(CASE WHEN vehicles.type_vehicle_id = 1 AND DATE(vehicles.created_at) = CURDATE() THEN 1 ELSE 0 END) as today_motor_count'),
+                DB::raw('SUM(CASE WHEN vehicles.type_vehicle_id = 2 AND DATE(vehicles.created_at) = CURDATE() THEN 1 ELSE 0 END) as today_car_count'),
+                DB::raw('SUM(CASE WHEN vehicles.type_vehicle_id = 3 AND DATE(vehicles.created_at) = CURDATE() THEN 1 ELSE 0 END) as today_bus_count'),
+                DB::raw('SUM(CASE WHEN vehicles.type_vehicle_id = 4 AND DATE(vehicles.created_at) = CURDATE() THEN 1 ELSE 0 END) as today_truck_count'),
                 //30min
                 DB::raw('SUM(CASE WHEN vehicles.created_at >= NOW() - INTERVAL 30 MINUTE THEN 1 ELSE 0 END) as recent_vehicle_count'),
                 DB::raw('SUM(CASE WHEN vehicles.type_vehicle_id = 1 AND vehicles.created_at >= NOW() - INTERVAL 30 MINUTE THEN 1 ELSE 0 END) as recent_motor_count'),
@@ -37,16 +52,6 @@ class DashboardController extends Controller
             )
             ->groupBy('spbus.spbu_id', 'spbus.name', 'spbus.latitude', 'spbus.longitude')
             ->get();
-            
-        foreach ($islands as $island) {
-            $count = Spbu::where('island_id', $island->island_id)->count();
-            $island->where('island_id', $island->island_id)->update(['count' => $count]);
-        }
-
-        foreach ($provinces as $province) {
-            $count = Spbu::where('province_id', $province->province_id)->count();
-            $province->where('province_id', $province->province_id)->update(['count' => $count]);
-        }
         
         return Inertia::render('Dashboard', [
             'islands' => $islands,
