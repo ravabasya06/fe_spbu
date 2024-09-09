6856963950243abadae97853562d6a3275d226c1
Adreon <sachioramzy12@gmail.com>
2024-09-09 16:57:34 +0700

    styling the pagination links

--- resources/js/Components/Analysis/Pagination.vue
+++ resources/js/Components/Analysis/Pagination.vue
@@ -1,9 +1,42 @@
 <script setup>
 defineProps(["links"]);
 </script>
+
 <template>
-    <Link v-if="links" v-for="link in links" :href="link.url">
-        {{ link.label }}
-    </Link>
+    <div class="page">
+        <Link
+            v-if="links"
+            v-for="link in links"
+            :href="link.url"
+            :style="{
+                backgroundColor: link.active ? 'blue' : 'none',
+                borderColor: link.active ? 'blue' : 'none',
+            }"
+        >
+            <span v-html="link.label"></span>
+        </Link>
+    </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+.page {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+    justify-content: center;
+}
+a {
+    text-decoration: none;
+    color: white;
+    border: 1px solid white;
+    border-radius: 3px;
+    padding: 10px;
+}
+a:hover {
+    border: 1px solid blue;
+    background-color: blue;
+}
+.page a.active {
+    border: 1px solid blue;
+}
+</style>
