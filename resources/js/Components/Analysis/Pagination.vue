406e8aeeb0bf89074b0443e9057cabc847453129
ravabasya06 <ravabasya06@gmail.com>
2024-09-09 15:44:10 +0700

    added pagination for spbu

---
+++ resources/js/Components/Analysis/Pagination.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps(["links"]);
+</script>
+<template>
+    <Link v-if="links" v-for="link in links" :href="link.url">
+        {{ link.label }}
+    </Link>
+</template>
+<style scoped></style>
