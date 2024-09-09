9920b89862d2580d65a2324db0eb864012c46db3
ravabasya06 <ravabasya06@gmail.com>
2024-09-09 15:45:04 +0700

    added pagination for results

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Layout from "../Components/Main/Layout.vue";
 import AnalysisCard from "../Components/Analysis/AnalysisCard.vue";
+import Pagination from "../Components/Analysis/Pagination.vue";
 
 defineProps(["spbus", "query", "results"]);
 </script>
@@ -8,5 +9,6 @@ defineProps(["spbus", "query", "results"]);
 <template>
     <Layout title="Analysis">
         <AnalysisCard :spbus="spbus" :results="results" />
+        <Pagination v-if="results.links" :links="results.links" />
     </Layout>
 </template>
